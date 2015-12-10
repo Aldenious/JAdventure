034346762127d688736e597be5aa5605e0fc6b08
BuildTools <aaaabaacca@gmail.com>
2015-12-10 22:36:58 +1300

    Added Jdoc and Comments to adventure.view view.AdventureFrame, and view.AdventureCanvas have full Documentation on all methods, as well as some field commenting.

--- src/adventure/view/AdventureCanvas.java
+++ src/adventure/view/AdventureCanvas.java
@@ -9,29 +9,58 @@ import java.awt.GraphicsEnvironment;
 import adventure.model.Game;
 import adventure.model.RoomSection;
 
-public class AdventureCanvas extends Canvas
-{
-	private final Game game;
-	private RoomSection View;
-	
-	public AdventureCanvas(Game game) 
-	{
+/**
+ * AdventureCanvas provides the actual graphics panel to draw and house the
+ * graphics for adventure. It is responsible for reporting to it's
+ * AdventureFrame the size of the graphics, as well as providing the methods for
+ * the individual graphical sections contained in the game model to print each
+ * individual object.
+ * 
+ * @author Aidan
+ *
+ */
+public class AdventureCanvas extends Canvas {
+	private final Game game; // Game reference for canvas to draw
+	private RoomSection View; // References the current view to draw which is
+								// the RoomSection that the player is viewing,
+								// inherited from the game.
+
+	/**
+	 * Creates and localizes the graphics canvas. Setting the size of the new
+	 * canvas to the graphic's minimum size.
+	 * 
+	 * @param game
+	 *            The game model for the canvas to reference for drawing, should
+	 *            be Inherited from AdventureFrame.
+	 */
+	public AdventureCanvas(Game game) {
 		this.game = game;
 		this.View = game.getPlayer().getViewing();
-		setSize(new Dimension(View.getSizeX(),View.getSizeY()));
-		GraphicsEnvironment env = GraphicsEnvironment.getLocalGraphicsEnvironment();
+		setSize(new Dimension(View.getSizeX(), View.getSizeY()));
+		GraphicsEnvironment env = GraphicsEnvironment
+				.getLocalGraphicsEnvironment();
 	}
-	
-	public void paint(Graphics g) 
-	{	
+
+	/**
+	 * Method called whenever any changes to the view may be altered, such as
+	 * interacting with objects, or moving around a room. Repaints the
+	 * background to be black first of all, in case the background is not
+	 * square, then calls view's draw method which recursively draws all other
+	 * object's in the RoomSection.
+	 */
+	public void paint(Graphics g) {
 		View = game.getPlayer().getViewing();
 		g.setColor(Color.BLACK);
-		g.fillRect(0,0,View.getSizeX(),View.getSizeY());
+		g.fillRect(0, 0, View.getSizeX(), View.getSizeY());
 		View.draw(g);
 	}
-	
+
+	/**
+	 * This method acts as a way to set the preferred size of the window whenever
+	 * AdventureFrame requests the size of AdventureCanvas.
+	 */
 	@Override
-    public Dimension getPreferredSize() {
-    	return new Dimension(View.getSizeX(), View.getSizeY());
-    }
+	public Dimension getPreferredSize() {
+		return new Dimension(View.getSizeX(), View.getSizeY());
+	}
 }
--- src/adventure/view/AdventureFrame.java
+++ src/adventure/view/AdventureFrame.java
@@ -10,21 +10,46 @@ import javax.swing.JFrame;
 
 import adventure.model.Game;
 
+/**
+ * AdventureFrame is the main component for the View component in MVC. It is
+ * responsible for housing, creating, and handling the window specifications of
+ * the view. It delagates the actual graphical tasks to be printed to the screen
+ * to it's internal AdventureCanvas.
+ * 
+ * @author Aidan
+ *
+ */
 public class AdventureFrame extends JFrame {
-	private final AdventureCanvas canvas;
+	private final AdventureCanvas canvas; // The actual graphics canvas
+											// adventure will draw too.
 
+	/**
+	 * Creates a new graphics frame for adventure. THIS SHOULD ONLY BE CALLED BY
+	 * WORLD. Instantiates and links up with the model to make sure that it is
+	 * displaying the correct information, and creates a new AdventureCanvas to
+	 * link to itself, and display the graphics. Finally, it assigns both mouse
+	 * and key listeners to the Adventure Canvas to listen to mouse, and key
+	 * events. 
+	 * 
+	 * @param title
+	 * 			title for the frame to reference.
+	 * @param game
+	 * 			the game model for the view to reference and draw.
+	 * @param mouse
+	 * 			A mouse listener to listen to mouse events on the canvas.
+	 * @param keys
+	 * 			A key listener to listen to key events on the canvas.
+	 */
 	public AdventureFrame(String title, Game game, MouseListener mouse,
-			KeyListener... keys) {
+			KeyListener key) {
 		super(title);
 
 		canvas = new AdventureCanvas(game);
 		setLayout(new BorderLayout());
-		if (keys != null) {
-			for (KeyListener k : keys) {
-				canvas.addKeyListener(k);
-			}
+		if (key != null && mouse != null) { //safety checks for the listeners
+			canvas.addKeyListener(key);
+			canvas.addMouseListener(mouse);
 		}
-		canvas.addMouseListener(mouse);
 		add(canvas, BorderLayout.CENTER);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
@@ -33,6 +58,13 @@ public class AdventureFrame extends JFrame {
 		canvas.requestFocus();
 	}
 
+	/**
+	 * Repaints the graphics, and resizes the screen to contain the size of the
+	 * new room being painted It First shifts the screen toward the center of
+	 * the display, and then resizes the window so that the window is
+	 * Appropriately sized to fit the bounds of the current displayed objects in
+	 * a RoomSection Finally, it calls the canvas to redraw it's graphics.
+	 */
 	public void repaint() {
 		// Center window in screen
 		Toolkit toolkit = Toolkit.getDefaultToolkit();
@@ -41,7 +73,7 @@ public class AdventureFrame extends JFrame {
 				(scrnsize.height - getHeight()) / 2, getWidth(), getHeight());
 		setResizable(true);
 		pack();
-		
+
 		canvas.repaint();
 	}
 }
